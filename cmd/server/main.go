3be8e871740ec366fb29c9410852c8c9658383f7
Tiago Carioca <tiagoacarioca@gmail.com>
2024-02-17 18:44:51 -0400

    feat: Add sqlx for database connection and repository pattern

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
-
-	"database/sql"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/lucsky/cuid"
 )
+
+type KeyParam struct {
+	Key string `param:"key"`
+}
 
 type Link struct {
 	ID  string `json:"id,omitempty"`
@@ -23,7 +27,11 @@ type Link struct {
 }
 
 type DB struct {
-	*sql.DB
+	*sqlx.DB
+}
+
+type LinkRepository struct {
+	db *DB
 }
 
 func customLogger() echo.MiddlewareFunc {
@@ -41,20 +49,36 @@ func main() {
 	}
 
 	db, err := NewDB()
-
 	if err != nil {
 		slog.Error("Error initializing database connection", "error", err)
 		os.Exit(1)
 	}
+	slog.Info("Connected to database")
 
 	defer db.Close()
+
+	linkRepository := NewLinkRepository(db)
 
 	e := echo.New()
 
 	e.Use(customLogger())
 	e.Use(middleware.Recover())
 
-	// e.GET("/:shortKey")
+	e.GET("/:key", func(c echo.Context) error {
+		var params KeyParam
+
+		if err := c.Bind(&params); err != nil {
+			return err
+		}
+
+		link, err := linkRepository.GetLinkByKey(params.Key)
+		if err != nil {
+			slog.Error("Error get linking by key", err)
+		}
+
+		return c.Redirect(http.StatusPermanentRedirect, link.URL)
+
+	})
 
 	api := e.Group("/api")
 
@@ -67,14 +91,6 @@ func main() {
 	v1 := api.Group("/v1")
 
 	links := v1.Group("/links")
-	//
-	links.GET("", func(c echo.Context) error {
-		return c.NoContent(http.StatusNoContent)
-	})
-
-	links.GET("/:id", func(c echo.Context) error {
-		return c.String(http.StatusOK, c.Param(":id"))
-	})
 
 	links.POST("", func(c echo.Context) error {
 		var link Link
@@ -86,25 +102,28 @@ func main() {
 		link.ID = cuid.New()
 		link.Key = cuid.Slug()
 
+		err := linkRepository.CreateLink(&link)
+		if err != nil {
+			slog.Error("Error creating link", err)
+		}
+
+		c.Response().Header().Set("Location", fmt.Sprintf("/%s", link.Key))
+
 		return c.JSON(http.StatusOK, link)
 	})
 
-	links.DELETE("/:id", func(c echo.Context) error {
-		return c.NoContent(http.StatusNoContent)
-	})
-
 	e.Logger.Fatal(e.Start(":3333"))
-
 }
 
 func NewDB() (*DB, error) {
-
-	db, err := sql.Open("mysql", os.Getenv("DB_URL"))
+	db, err := sqlx.Connect("mysql", os.Getenv("DB_URL"))
 	if err != nil {
 		return nil, err
 	}
 
-	err = db.Ping()
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
 
 	db.SetConnMaxLifetime(time.Minute * 3)
 	db.SetMaxOpenConns(10)
@@ -115,4 +134,31 @@ func NewDB() (*DB, error) {
 
 func (db *DB) Close() {
 	db.DB.Close()
+}
+
+func NewLinkRepository(db *DB) *LinkRepository {
+	slog.Info("New Link Repository")
+	return &LinkRepository{
+		db,
+	}
+}
+
+func (lr *LinkRepository) CreateLink(link *Link) error {
+	_, err := lr.db.Exec("INSERT INTO links (id, `key`, url) VALUES (?, ?, ?)", link.ID, link.Key, link.URL)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (lr *LinkRepository) GetLinkByKey(key string) (*Link, error) {
+	var link Link
+
+	err := lr.db.Get(&link, "SELECT * FROM links WHERE `key`=?", key)
+	if err != nil {
+		return &link, err
+	}
+
+	return &link, nil
 }
